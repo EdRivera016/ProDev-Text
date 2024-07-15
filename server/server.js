35bbafd01c78ddced6d90a905acb643f27efc3a3
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-07-14 22:47:35 -0400

    testing refactor for correct deployment

--- server/server.js
+++ server/server.js
@@ -15,15 +15,17 @@
 // });
 const express = require('express');
 const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Now listening on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
